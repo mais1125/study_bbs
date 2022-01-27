6c5bf598c45376d10a1827d21aae06f8f58b26f7
pyphy <pyphy@usepm.com>
2022-01-27 16:40:47 +0000

    カテゴリーページ編集、ページ遷移修正

--- apps/web/src/app/pages/main/main.component.ts
+++ apps/web/src/app/pages/main/main.component.ts
@@ -13,25 +13,6 @@ export class MainComponent implements OnInit {
   threads: Thread[] = [];
   categoryies: Category[] = [];
 
-  // // 求めるデータ
-  // categoryies = [
-  //   {
-  //     id: 1,
-  //     name: 'カテゴリA',
-  //     theards: [{}, {}, {}, {}, {}],
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'カテゴリB',
-  //     theards: [{}, {}, {}, {}, {}],
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'カテゴリC',
-  //     theards: [{}, {}, {}, {}, {}],
-  //   },
-  // ];
-
   constructor(private apiService: ApiService, public router: Router) {}
 
   async ngOnInit(): Promise<void> {
@@ -51,7 +32,6 @@ export class MainComponent implements OnInit {
   onClick(id: Thread): void {
     this.router.navigate([PAGE.THREAD], {
       queryParams: { id: id.id },
-      queryParamsHandling: 'merge',
     });
   }
 }
